3c8f0448b69c9135ea32b0eb7f59f133426e6373
xiaodianqinhan <88314846+xiaodianqinhan@users.noreply.github.com>
2023-11-11 23:09:37 +0800

    init spring boot project

---
+++ src/main/java/org/sleepy/hmmusicbox/HmmusicboxApplication.java
@@ -0,0 +1,13 @@
+package org.sleepy.hmmusicbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmmusicboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmmusicboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sleepy/hmmusicbox/HmmusicboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sleepy.hmmusicbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmmusicboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
