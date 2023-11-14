2efff570639617d3055a3964357d465cb093124c
lzh <211870018@smail.nju.edu.cn>
2023-11-14 23:19:23 +0800

    test

--- src/main/java/org/sleepy/hmmusicbox/dao/UserDao.java
+++ src/main/java/org/sleepy/hmmusicbox/dao/UserDao.java
@@ -4,4 +4,5 @@ import org.sleepy.hmmusicbox.pojo.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
 }
